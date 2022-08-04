6e2bfd6443be7162877d4c4fce0d619753c81c31
Hakan Ergun <hakan.ergun@ieee.org>
2022-08-04 12:41:32 +0200

    Fix in the inertia constraint

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -2,5 +2,5 @@ function constraint_inertia_limit(pm::_PM.AbstractDCPModel, n::Int, gen_inertia,
     pg    = _PM.var(pm, n, :pg)
     pconv_grid_ac = _PM.var(pm, n,  :pconv_tf_fr)
 
-    JuMP.@constraint(pm.model, sum([pg[g] * iner_g  for (g, iner_g) in gen_inertia]) + sum([pconv_grid_ac[c] * iner_c  for (c, iner_c) in conv_inertia])  >= inertia_limit) #
+    JuMP.@constraint(pm.model, sum([pg[g] * iner_g  for (g, iner_g) in gen_inertia]) - sum([pconv_grid_ac[c] * iner_c  for (c, iner_c) in conv_inertia])  >= inertia_limit) #
 end
