39ae10456bab978d6d33d1660d732c2628cbb8a6
Hakan Ergun <hakan.ergun@ieee.org>
2023-06-19 11:05:50 +1000

    Relaxed MUT & MDT binaries for UC problem

--- examples/frequency_constrained_uc.jl
+++ examples/frequency_constrained_uc.jl
@@ -12,7 +12,7 @@ import LaTeXStrings
 
 
 # Load test file
-file = "./test/data/case5_2grids_inertia_acdc.m"
+file = "./test/data/case5_3grids_inertia_hvdc.m"
 plot_path = "/Users/hergun/Library/CloudStorage/OneDrive-KULeuven/Projects/HVDC_H2/plots"
 
 # Define solvers
@@ -28,16 +28,21 @@ CbaOPF.prepare_data!(data; t_hvdc = 0.10, ffr_cost = 50.0)
 fmin = 49.0:0.05:49.8
 objective_no_dc = zeros(1, length(fmin))
 objective_dc = zeros(1, length(fmin))
+objective_no_dc_relax = zeros(1, length(fmin))
+objective_dc_relax = zeros(1, length(fmin))
 
 number_of_hours = 24
 
 g_series = [0.4  0.5  0.66  0.7   0.7   0.9   0.95 1.02  1.15  1.3   1.35 1.3   1.21  1.08  1.0  0.96  0.93 1.0  1.1   1.2  1.08  1.05  0.99 0.89]
+# g_series = [1.02  1.15  1.3   1.35 1.3   1.21  1.08  1.0 0.4  0.5  0.66  0.7   0.7   0.9   0.95   0.96  0.93 1.0  1.1   1.2  1.08  1.05  0.99 0.89]
 l_series = [0.6  0.7  0.75  0.78  0.85  0.88  0.9  1.0   1.12  1.25  1.2  1.08  0.99  0.92  0.8  0.73  0.8  0.9  1.03  1.2  1.11  0.99  0.8  0.69]
 # l_series = ones(1,24) .* 0.75
 # g_series = ones(1,24) .* 2
 
 result_no_dc = Dict{String, Any}(["$idx"=>Dict{String, Any}() for idx = 1:length(fmin)])
 result_dc = Dict{String, Any}(["$idx"=>Dict{String, Any}() for idx = 1:length(fmin)])
+result_no_dc_relax = Dict{String, Any}(["$idx"=>Dict{String, Any}() for idx = 1:length(fmin)])
+result_dc_relax = Dict{String, Any}(["$idx"=>Dict{String, Any}() for idx = 1:length(fmin)])
 for idx = 1:length(fmin)
     # Add frequency stability data
     data["frequency_parameters"] = Dict{String, Any}()
@@ -49,21 +54,32 @@ for idx = 1:length(fmin)
     # Add generator contingencies
     mn_data = CbaOPF.create_multinetwork_model!(data, number_of_hours, g_series, l_series)
     # Provide addtional settings as part of the PowerModels settings dictionary
-    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true, "hvdc_inertia_contribution" => false)
+    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true, "hvdc_inertia_contribution" => false, "relax_uc_binaries" => false)
     result_no_dc["$idx"] = CbaOPF.solve_fsuc(mn_data, _PM.DCPPowerModel, gurobi, setting = s, multinetwork = true)
     objective_no_dc[1, idx] = result_no_dc["$idx"]["objective"] 
 
-    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true, "hvdc_inertia_contribution" => true)
+    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true, "hvdc_inertia_contribution" => false, "relax_uc_binaries" => true)
+    result_no_dc_relax["$idx"] = CbaOPF.solve_fsuc(mn_data, _PM.DCPPowerModel, gurobi, setting = s, multinetwork = true)
+    objective_no_dc_relax[1, idx] = result_no_dc_relax["$idx"]["objective"] 
+
+    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true, "hvdc_inertia_contribution" => true, "relax_uc_binaries" => false)
     result_dc["$idx"] = CbaOPF.solve_fsuc(mn_data, _PM.DCPPowerModel, gurobi, setting = s, multinetwork = true)
     objective_dc[1, idx] = result_dc["$idx"]["objective"] 
+
+    s = Dict("output" => Dict("branch_flows" => true), "conv_losses_mp" => true, "hvdc_inertia_contribution" => true, "relax_uc_binaries" => true)
+    result_dc_relax["$idx"] = CbaOPF.solve_fsuc(mn_data, _PM.DCPPowerModel, gurobi, setting = s, multinetwork = true)
+    objective_dc_relax[1, idx] = result_dc_relax["$idx"]["objective"] 
 end
 
-p1 = Plots.plot(fmin, objective_no_dc', xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~€\$", label = "without HVDC contribution")
-Plots.plot!(p1, fmin, objective_dc',  xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~€\$", label = "with HVDC contribution")
+p1 = Plots.plot(fmin, objective_no_dc'/1e6, xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~M€\$", label = "without HVDC contribution")
+Plots.plot!(p1, fmin, objective_dc'/1e6,  xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~M€\$", label = "with HVDC contribution")
+Plots.plot!(p1, fmin, objective_no_dc_relax'/1e6,  xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~M€\$", label = "without HVDC contribution - relax")
+Plots.plot!(p1, fmin, objective_dc_relax'/1e6,  xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~M€\$", label = "with HVDC contribution - relax")
 plot_filename = joinpath(plot_path, "objective_comparison.pdf")
 Plots.savefig(p1, plot_filename)
 
-f_idx = 5
+f_idx = 9
+mn_data = CbaOPF.create_multinetwork_model!(data, number_of_hours, g_series, l_series)
 number_of_generators = length(data["gen"])
 alpha_dc = zeros(number_of_hours, number_of_generators)
 alpha_p_dc = zeros(number_of_hours, number_of_generators)
@@ -96,29 +112,32 @@ for i in 1:size(alpha_dc, 1)
     end
 end
 
-p3 = Plots.scatter(alpha_p_dc,color=color_dc,legend=nothing, xlabel = "\$hour\$", ylabel = "\$g_{id}\$")
+p3 = Plots.scatter(alpha_p_dc,color=color_dc,legend=nothing, xlabel = "\$hour\$", ylabel = "\$g_{id}\$", title = "generator status with HVDC contribution")
 plot_filename = joinpath(plot_path, "generator_status_dc.pdf")
 Plots.savefig(p3, plot_filename)
 
-p4 = Plots.scatter(alpha_p_no_dc,color=color_no_dc,legend=nothing, xlabel = "\$hour\$", ylabel = "\$g_{id}\$")
+p4 = Plots.scatter(alpha_p_no_dc,color=color_no_dc,legend=nothing, xlabel = "\$hour\$", ylabel = "\$g_{id}\$", title = "generator status without HVDC contribution")
 plot_filename = joinpath(plot_path, "generator_status_no_dc.pdf")
 Plots.savefig(p4, plot_filename)
 
-fmin_idx = f_idx
-mn_data = CbaOPF.create_multinetwork_model!(data, number_of_hours, g_series, l_series)
-print("########### NO HVDC contribution for fmin = ", fmin[fmin_idx], " Hz #####################", "\n")
-CbaOPF.print_frequency_information(result_no_dc, mn_data; fmin_idx = fmin_idx)
-print("########### With HVDC contribution for fmin = ", fmin[fmin_idx], " Hz #####################", "\n")
-CbaOPF.print_frequency_information(result_dc, mn_data; fmin_idx = fmin_idx)
+p5 = Plots.plot(1:number_of_hours,g_series', xlabel = "\$hour\$", ylabel = "\$profile in p.u.\$", label = "RES generation profile")
+Plots.plot!(p5, 1:number_of_hours,l_series', xlabel = "\$hour\$", ylabel = "\$profile in p.u.\$", label = "demand profile")
+plot_filename = joinpath(plot_path, "profiles.pdf")
+Plots.savefig(p5, plot_filename)
+
+p2 = Plots.plot(fmin, tie_line_flows, xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Flow~in~MVA\$", label = "tie line flow zone 1 -> zone 2, without HVDC contribution")
+Plots.plot!(p2, fmin, tie_line_flows_dc,  xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~€\$", label = "tie line flow zone 1 -> zone 2, with HVDC contribution")
+plot_filename = joinpath(plot_path, "tieline_flows.pdf")
+Plots.savefig(p2, plot_filename)
+
+# fmin_idx = f_idx
+# print("########### NO HVDC contribution for fmin = ", fmin[fmin_idx], " Hz #####################", "\n")
+# CbaOPF.print_frequency_information(result_no_dc, mn_data; fmin_idx = fmin_idx)
+# print("########### With HVDC contribution for fmin = ", fmin[fmin_idx], " Hz #####################", "\n")
+# CbaOPF.print_frequency_information(result_dc, mn_data; fmin_idx = fmin_idx)
 # br_idx = data["tie_lines"]["1"]["br_idx"]
 # tie_line_flows = [result_no_dc["$idx"]["solution"]["nw"]["1"]["branch"]["$br_idx"]["pf"] for idx in 1:length(fmin)] * data["baseMVA"]
 # tie_line_flows_dc = [result_dc["$idx"]["solution"]["nw"]["1"]["branch"]["$br_idx"]["pf"] for idx in 1:length(fmin)] * data["baseMVA"]
-
-# p2 = Plots.plot(fmin, tie_line_flows, xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Flow~in~MVA\$", label = "tie line flow zone 1 -> zone 2, without HVDC contribution")
-# Plots.plot!(p2, fmin, tie_line_flows_dc,  xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~€\$", label = "tie line flow zone 1 -> zone 2, with HVDC contribution")
-# plot_filename = joinpath(plot_path, "tieline_flows.pdf")
-# Plots.savefig(p2, plot_filename)
-
 
 # p1 = Plots.plot(fmin, objective_no_dc', xlabel = "\$f_{min} in~Hz\$", ylabel = "\$Cost~in~€\$", label = "total cost")
 # plot_filename = joinpath(plot_path, "objective_comparison.pdf")
--- src/core/variable.jl
+++ src/core/variable.jl
@@ -182,8 +182,13 @@ end
 function variable_generator_states(pm::_PM.AbstractPowerModel; nw::Int=_PM.nw_id_default, uc = false)
     variable_generator_state(pm, nw = nw)
     if uc == true
-        variable_generator_state_mut(pm, nw = nw)
-        variable_generator_state_mdt(pm, nw = nw)
+        if haskey(pm.setting, "relax_uc_binaries") && pm.setting["relax_uc_binaries"] == true
+            variable_generator_state_mut_relax(pm, nw = nw)
+            variable_generator_state_mdt_relax(pm, nw = nw)
+        else
+            variable_generator_state_mut(pm, nw = nw)
+            variable_generator_state_mdt(pm, nw = nw)
+        end
     end
 end
 
@@ -214,6 +219,30 @@ function variable_generator_state_mdt(pm::_PM.AbstractPowerModel; nw::Int=_PM.nw
     gamma_g = _PM.var(pm, nw)[:gamma_g] = JuMP.@variable(pm.model,
         [i in _PM.ids(pm, nw, :gen)], base_name="$(nw)_gamma_g",
         binary = true,
+        start = 0
+    )
+    report && _PM.sol_component_value(pm, nw, :gen, :gamma_g, _PM.ids(pm, nw, :gen), gamma_g)
+end
+
+"Variable for minimum up-time relaxed"
+function variable_generator_state_mut_relax(pm::_PM.AbstractPowerModel; nw::Int=_PM.nw_id_default, bounded::Bool=true, report::Bool=true)
+    beta_g = _PM.var(pm, nw)[:beta_g] = JuMP.@variable(pm.model,
+        [i in _PM.ids(pm, nw, :gen)], base_name="$(nw)_beta_g",
+        binary = false,
+        lower_bound = 0,
+        upper_bound = 1,
+        start = 0
+    )
+    report && _PM.sol_component_value(pm, nw, :gen, :beta_g, _PM.ids(pm, nw, :gen), beta_g)
+end
+
+"Variable for minimum down-time relaxed"
+function variable_generator_state_mdt_relax(pm::_PM.AbstractPowerModel; nw::Int=_PM.nw_id_default, bounded::Bool=true, report::Bool=true)
+    gamma_g = _PM.var(pm, nw)[:gamma_g] = JuMP.@variable(pm.model,
+        [i in _PM.ids(pm, nw, :gen)], base_name="$(nw)_gamma_g",
+        binary = false,
+        lower_bound = 0,
+        upper_bound = 1,
         start = 0
     )
     report && _PM.sol_component_value(pm, nw, :gen, :gamma_g, _PM.ids(pm, nw, :gen), gamma_g)
