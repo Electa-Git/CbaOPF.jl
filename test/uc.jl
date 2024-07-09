2201bef45b9a4a0ef0d054fa1f9601c12d76cee8
Hakan Ergun <hakan.ergun@ieee.org>
2024-07-09 14:29:35 +0200

    Trigger action test

--- test/uc.jl
+++ test/uc.jl
@@ -52,6 +52,7 @@ highs = JuMP.optimizer_with_attributes(HiGHS.Optimizer)
     end
 end
 
+# 
 @testset "2 Grids ACDC" begin
     file = "../test/data/case5_2grids_inertia_acdc.m"
     # Parse file using PowerModels
